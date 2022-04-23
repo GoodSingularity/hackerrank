02c04d4bf198432af303056b2988364c12b1b8ca
GoodSingularity <ZMosciszko@protonmail.com>
2022-04-23 20:56:40 +0200

    Birthday-Cake Problem solution Update

---
+++ Birthday-Cake-Candles/solution.rb
@@ -0,0 +1,23 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles)
+    maxHeight = candles.max
+    return candles.count(maxHeight)
+
+end
+
+candles_count = gets.strip.to_i
+
+candles = gets.rstrip.split.map(&:to_i)
+
+print birthdayCakeCandles candles
